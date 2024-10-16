508ed9f4de08d11bb0c6d69e504a135fc087bf97
Matheus Tiburcio <matheusgtiburcio@gmail.com>
2024-10-16 18:37:43 -0300

    initial commit

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,25 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+
+    plugins: [
+      "module-resolver",
+      {
+        root: ["./src"],
+        alias: {
+          "@assets": "./src/assets",
+          "@components": "./src/components",
+          "@screens": "./src/screens",
+          "@routes": "./src/routes",
+          "@storage": "./src/storage",
+          "@types": "./src/types",
+          "@utils": "./src/utils",
+        },
+      },
+    ],
   };
 };
---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export function Groups() {
+  return (
+    <View className="flex items-center justify-center h-full">
+      <Text className="text-red-600 font-bold">Groups</Text>
+    </View>
+  );
+}
